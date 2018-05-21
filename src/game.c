32527c5feeafa9d1b76e80a41a7944036e50bffd
giillyan.como <giillyan.como@localhost.localdomain>
2018-05-21 16:52:09 +0200

    add gestion longeur, win

--- src/game.c
+++ src/game.c
@@ -9,16 +9,24 @@
 #include <stdio.h>
 #include <string.h>
 
+int who_win(str_t *p, char *line)
+{
+	if (strcmp(line, p->sol)) {
+		printf("You win!\n");
+		exit (0);
+	}
+}
+
 int check_word(str_t *p, char *line)
 {
 	size_t len = strlen(line);
 	
-	if (strlen(p->rep) < len) {
-		printf("Word too short. Retry\n");
+	if (strlen(line) < len) {
+		printf("Word too short. Retry\n>");
 		return (1);
 	}
-	if (strlen(p->rep) > len) {
-		printf("Word too long. Retry\n");
+	if (strlen(line) > len) {
+		printf("Word too long. Retry\n>");
 		return (1);
 	}
 	return (0);
@@ -33,14 +41,16 @@ int game2(str_t *p)
 	printf("*: invalid letter\n");
 	printf("?: incorrectly placed letter\n");
 	printf("Will you find the secret word?\n");
+	printf("%s\n\n", p->aff);
 
 	while (a != my_norm_strlen(p->sol)) {
 		a++;
-		printf("Round %d\n", a);
+		printf("Round %d\n>", a);
 		getline(&line, &len, stdin);
 		if (line[strlen(line) - 1] == '\n')
 			line[strlen(line) -1] = '\0';
-		if (check_word(p, line) == 1)
-			continue;
+		while (check_word(p, line) == 1)
+			getline(&line, &len, stdin);
+		who_win(p, line);
 	}
 }
